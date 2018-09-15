904a8c01721a139a88ca5eeef740834238f36c9d
Schelkunchik <shchelkunou.artem@gmail,com>
2018-09-15 22:08:39 +0300

    rss-school-second-task-love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var triangleCounter = 0;
+  for(var i = 0; i < preferences.length; i++) {
+    var firstLover = i+1;               // First Spichonee
+    var l1 = preferences[firstLover-1]; // First Spichonee loves Second Spichonee
+    var secondLover = l1;                // Second Spichonee
+    var l2 = preferences[secondLover-1]; // Second Spichonee loves Third Spichonee
+    var thirdLover = l2;                // Third Spichonee
+    var l3 = preferences[thirdLover-1]; // Third Spichonee loves First Spichonee
+
+    if((firstLover != l1) && (l3 == firstLover)) {
+      triangleCounter ++;
+    }
+  }
+
+  return triangleCounter/3;
 };
